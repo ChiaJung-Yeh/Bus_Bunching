e774c038566abb92e7ad3e9f50b8375ba06efa30
ChiaJung-Yeh <robert1328.mg10@nycu.edu.tw>
2025-10-17 14:20:36 +1100

    initial commit

--- GetRealtime.R
+++ GetRealtime.R
@@ -6,23 +6,32 @@ client_id="robert1328.mg10-5fef152e-ee4f-4cfb"
 client_secret="591fe8aa-6fdc-4c3b-9663-428a62ec8863"
 access_token=get_token(client_id, client_secret)
 
-TIME=as.POSIXct(Sys.time(), tz="Asia/Taipei")
-url="https://tdx.transportdata.tw/api/basic/v2/Bus/RealTimeNearStop/City/Taipei/307?&%24format=JSON"
-x=GET(url, add_headers(Accept="application/+json", Authorization=paste("Bearer", access_token)))
-bus_a2=fromJSON(content(x, as="text"))%>%
-  select(PlateNumb, RouteUID, RouteName, SubRouteUID, SubRouteName, Direction, StopUID, StopName, StopSequence, DutyStatus, BusStatus, A2EventType, GPSTime, TripStartTime, TripStartTimeType)
-bus_a2$RouteName=bus_a2$RouteName$Zh_tw
-bus_a2$SubRouteName=bus_a2$SubRouteName$Zh_tw
-bus_a2$StopName=bus_a2$StopName$Zh_tw
 
-write.csv(bus_a2, paste0("A2_Realtime/", gsub("-|:", "_", substr(as.character(TIME), 1, regexpr("\\.", as.character(TIME))-1)), ".csv"), row.names=F)
+repeat{
+  TIME=as.POSIXct(Sys.time(), tz="Asia/Taipei")
+  url="https://tdx.transportdata.tw/api/basic/v2/Bus/RealTimeNearStop/City/Taipei/307?&%24format=JSON"
+  x=GET(url, add_headers(Accept="application/+json", Authorization=paste("Bearer", access_token)))
+  bus_a2=fromJSON(content(x, as="text"))%>%
+    select(PlateNumb, RouteUID, RouteName, SubRouteUID, SubRouteName, Direction, StopUID, StopName, StopSequence, DutyStatus, BusStatus, A2EventType, GPSTime, TripStartTime, TripStartTimeType)
+  bus_a2$RouteName=bus_a2$RouteName$Zh_tw
+  bus_a2$SubRouteName=bus_a2$SubRouteName$Zh_tw
+  bus_a2$StopName=bus_a2$StopName$Zh_tw
+  
+  write.csv(bus_a2, paste0("A2_Realtime/", gsub("-|:", "_", substr(as.character(TIME), 1, regexpr("\\.", as.character(TIME))-1)), ".csv"), row.names=F)
+  
+  system("git init")
+  system("git add .")
+  # system('git config --global user.email "robert1328.mg10@nycu.edu.tw"')
+  # system('git config --global user.name "ChiaJung-Yeh"')
+  system('git commit -m "initial commit"')
+  system("git push origin main")
+  
+  # Log progress
+  message(format(Sys.time(), "%Y-%m-%d %H:%M:%S"), " — script executed")
+  
+  Sys.sleep(30)
+}
 
-system("git init")
-system("git add .")
-# system('git config --global user.email "robert1328.mg10@nycu.edu.tw"')
-# system('git config --global user.name "ChiaJung-Yeh"')
-system('git commit -m "initial commit"')
-system("git push origin main")
 
 
 
